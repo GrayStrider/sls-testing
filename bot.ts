84f6314c27f5b034ff66840306723b4ae5e33e60
Gray Strider <zhoga.ivan@gmail.com>
2019-10-30 02:23:59 +1100

    successful connect, return debug info

--- bot.ts
+++ bot.ts
@@ -1,4 +1,5 @@
 import {APIGatewayProxyHandler} from "aws-lambda";
+import * as Discord from 'discord.js'
 
 export const sayHelloGet: APIGatewayProxyHandler = async () => ({
 	statusCode: 200,
@@ -7,5 +8,24 @@ export const sayHelloGet: APIGatewayProxyHandler = async () => ({
 
 export const sayHelloPost: APIGatewayProxyHandler = async (event) => ({
 	statusCode: 200,
-	body: JSON.stringify(event)
+	body: JSON.stringify(event.body)
 })
+
+const TOKEN = 'NjM4NjgwNjQxNzM4NzY4NDE5.XbhIqw.DP5SLqPksUxRNqMjJhrUxaD5K0o'
+
+export const initializeBot: APIGatewayProxyHandler = async () => {
+	const client = new Discord.Client();
+	const messages: string[] = []
+	
+	client.on(
+		'debug', (event) =>
+			messages.push(event)
+	);
+	
+	await client.login(TOKEN);
+	
+	return {
+		statusCode: 200,
+		body: JSON.stringify(messages)
+	}
+}
