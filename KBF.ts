c73edde93f746df747dd2107439c889c838bf579
Gray Strider <zhoga.ivan@gmail.com>
2019-10-30 04:14:10 +1100

    add params, fetch tasks

--- KBF.ts
+++ KBF.ts
@@ -3,17 +3,23 @@ import axios from 'axios'
 const key = '917d43a34d814658933f908901d625a6'
 const key64 = Buffer.from('apiToken:' + key).toString('base64')
 
-const getKBF = async (base64_credentials: string, resource: string) => {
-	const response = await axios.get(`https://kanbanflow.com/api/v1/${resource}`,
+const response = async (base64_credentials: string, resource: string) => {
+
+}
+
+
+const kanbanGet = async (base64_credentials: string, resource: string, params?: { [key: string]: string }) =>
+	await axios.get(`https://kanbanflow.com/api/v1/${resource}`,
 		{
 			headers: {
 				'Authorization': `Basic ${base64_credentials}`
-			}
+			},
+			params
 		})
-	
-	return response
-}
 
-getKBF(key64, 'users')
+
+kanbanGet(key64, 'tasks', {
+	columnId: 'kW2QjCrlUgJg'
+})
 	.then((res) => console.log(res.data))
 	.catch((err) => console.log(err.message))
