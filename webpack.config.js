b87a80f45ffa72bc40cd105ce257778b6f718f4a
Gray Strider <zhoga.ivan@gmail.com>
2019-10-30 00:11:44 +1100

    remove sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const slsw = require('serverless-webpack');
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  devtool: 'source-map',
+  // devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
