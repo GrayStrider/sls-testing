7efa26a228e255a8858071197242616056da882e
Gray Strider <zhoga.ivan@gmail.com>
2019-10-30 03:15:37 +1100

    responder

--- bot-node.ts
+++ bot-node.ts
@@ -1,30 +1,47 @@
 const TOKEN = 'NjM4NjgwNjQxNzM4NzY4NDE5.XbhIqw.DP5SLqPksUxRNqMjJhrUxaD5K0o'
 import * as Discord from 'discord.js'
+import chalk from 'chalk'
+
+const log = console.log
+const config = {
+	prefix: '++'
+}
 
 export const initializeBot = async () => {
-	const client = new Discord.Client();
+	const client = new Discord.Client()
 	const messages: string[] = []
 	
 	client.on('ready', () =>
-		console.log('Ready!')
-	);
+		log('Ready!')
+	)
 	
-	client.on(
-		'debug', (event) => {
-			messages.push(event)
-			// console.log('DEBUG:' + event);
-		});
+	client.on('debug', (event) => {
+		messages.push(event)
+		log(chalk.yellow('DEBUG:' + event))
+	})
 	
-	await client.login(TOKEN);
-	return messages
+	client.on('message', (message) => {
+			log(chalk.cyanBright(message.content))
+			
+			if (message.author.bot) return
+			if (message.content.indexOf(config.prefix) !== 0) return
+			
+			const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+			const command = args[0] !== '' ? args.shift()!.toLowerCase() : 'default'
+			
+			console.log(args, command)
+			message.reply([command, args])
+		}
+	)
 	
+	await client.login(TOKEN)
+	// return messages
 }
 
-const main = async () => {
-	const messages = await initializeBot()
-	console.log(messages)
-	process.exit()
-}
 
-main()
-	.catch(console.log)
+initializeBot()
+	.then((data) => {
+		log(data)
+		// process.exit()
+	})
+	.catch((err) => log(chalk.red(err)))
