65809d96b93c51c851f6da9cebbba998eef0f4bb
Gray Strider <zhoga.ivan@gmail.com>
2019-10-30 03:52:50 +1100

    auth base64 header

--- KBF.ts
+++ KBF.ts
@@ -1,14 +1,19 @@
-import axios, {AxiosError} from 'axios'
+import axios from 'axios'
 
 const key = '917d43a34d814658933f908901d625a6'
-const APIKEY64 = new Buffer(key).toString('base64')
+const key64 = Buffer.from('apiToken:' + key).toString('base64')
 
-const getKBF = async (credentials: string, resource: string) => {
-	const response = await axios.get(`https://kanbanflow.com/api/v1/${resource}?apiToken=${credentials}`)
+const getKBF = async (base64_credentials: string, resource: string) => {
+	const response = await axios.get(`https://kanbanflow.com/api/v1/${resource}`,
+		{
+			headers: {
+				'Authorization': `Basic ${base64_credentials}`
+			}
+		})
 	
 	return response
 }
 
-getKBF(key, 'users')
-	.then(console.log)
-	.catch((err: AxiosError) => console.log(err.message))
+getKBF(key64, 'users')
+	.then((res) => console.log(res.data))
+	.catch((err) => console.log(err.message))
